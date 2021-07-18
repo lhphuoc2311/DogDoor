c3f81ce80362fd112658e6f3955b3dead2e9e6f5
Code Guru Mouse <phuoc2311@gmail.com>
2021-07-18 22:48:41 +0700

    second commit

--- src/vn/edu/giadinh/DogDoor.java
+++ src/vn/edu/giadinh/DogDoor.java
@@ -1,6 +1,24 @@
 package vn.edu.giadinh;
 
 public class DogDoor {
-    boolean open;
+    private boolean open;
+
+    public DogDoor(){
+        open = false;
+    }
+
+    public void open(){
+        System.out.println("Open the door");
+        open = true;
+    }
+
+    public void close(){
+        System.out.println("Close the door");
+        open = false;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
     
 }
--- src/vn/edu/giadinh/Remote.java
+++ src/vn/edu/giadinh/Remote.java
@@ -2,5 +2,17 @@ package vn.edu.giadinh;
 
 public class Remote {
     DogDoor door;
+
+    public Remote(DogDoor door){
+        this.door = door;
+    }
+
+    public void pressButton(){
+        if(door.isOpen()){
+            door.close();
+        }else{
+            door.open();
+        }
+    }
     
 }
