e3b36d617e7ada1a693057cde6dba8b76a02cafb
Code Guru Mouse <phuoc2311@gmail.com>
2021-07-19 11:16:02 +0700

    first commit

--- src/vn/edu/giadinh/DogDoorTestDrive.java
+++ src/vn/edu/giadinh/DogDoorTestDrive.java
@@ -9,6 +9,11 @@ public class DogDoorTestDrive {
         System.out.println("Todd and Gina hear Fido bark");
         System.out.println("Todd and Gina press Remote");
         remote.pressButton();
+        System.out.println("Fido get out");
+        System.out.println("Todd and Gina press Remote");
+        System.out.println("Do his business");
+        System.out.println();
+
         
     }
     
